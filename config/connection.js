7fef040123c15c616bb43ddf8a0fed341f413eef
Zack White <zhwhite18@yahoo.com>
2020-10-07 15:22:19 -0500

    working heroku out

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql");
 
-//const connection = mysql.createConnection({
-    //host: "localhost",
-    //port: 3306,
-    //user: "root",
-    //password: "Rebel91!",
-    //database: "burgers_db"
-//});
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Rebel91!",
+    database: "burgers_db"
+});
 
 //heroku mysql add-on connection
 if (process.env.JAWSDB_URL) {
