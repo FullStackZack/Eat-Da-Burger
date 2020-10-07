691f25452da7e1b77cb38c8141b5bd81488d9733
Zack White <zhwhite18@yahoo.com>
2020-10-07 15:26:59 -0500

    fixing connection

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,12 @@
 const mysql = require("mysql");
 
-const connection;
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Rebel91!",
+    database: "burgers_db"
+});
 
 //heroku mysql add-on connection
 if (process.env.JAWSDB_URL) {
